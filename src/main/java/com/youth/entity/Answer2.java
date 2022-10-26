d0a17696dd911a90e87d90fe3dc35b4364cb89ca
kimheeseok <gmltjr5179@gmail.com>
2022-10-26 15:08:46 +0900

    qna게시판

--- src/main/java/com/youth/entity/Answer2.java
+++ src/main/java/com/youth/entity/Answer2.java
@@ -1,17 +1,13 @@
 package com.youth.entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 
 import com.youth.entity.Member;
@@ -22,21 +18,18 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer2 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(length = 200)
-    private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+    @ManyToOne
+    private Question2 question2;
     
     @ManyToOne
     private Member author;
